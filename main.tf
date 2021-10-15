e02556027a1a5efb0019ad3de7a2ed2ecef6dd1d
Anh Quach (Tony) <anh.quachquituan@halliburton.com>
2021-10-15 17:40:39 +0700

    Update count for records

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ data "cloudflare_zone" "main_domain" {
 }
 
 resource "cloudflare_record" "aws_subdomain_dns" {
-  count      = 4
+  count      = length(var.name_servers)
   zone_id    = data.cloudflare_zone.main_domain.id
   name       = "${var.sub_domain}"
   value      = var.name_servers[count.index]
